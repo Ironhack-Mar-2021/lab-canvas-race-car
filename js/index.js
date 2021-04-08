5da91ac1ed0a9c3356f391b89856bd3b2792318a
linesofcode01 <frcoding@outlook.com>
2021-04-08 12:37:04 -0700

    section1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+const canvas = document.querySelector('#canvas')
+const ctx = canvas.getContext('2d')
+console.log(ctx)
 
-  function startGame() {}
-};
+// ctx.fillRect(0, 0, 50, 70)
+let roadImg = new Image()
+roadImg.src='./../images/road.png'
+roadImg.onload=function (e){
+  ctx.drawImage(roadImg, 0, 0, 500, 700)
+}
